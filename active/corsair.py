1c1a1be1a0e37e5440aa57fe2408eb42b2aac231
javixeneize <javi.xeneize@gmail.com>
2021-03-02 10:29:57 +0000

    Update corsair.py
    Comments updated
    
    Signed-off-by: javixeneize <javi.xeneize@gmail.com>

--- active/corsair.py
+++ active/corsair.py
@@ -1,9 +1,11 @@
 
-'''
+"""
+
 This is an adaptation of Corsair Scan (https://github.com/Santandersecurityresearch/corsair_scan) as a ZAP plugin.
 This is delivered as an active scan plugin that will resend requests to all the sites captured in ZAP, injecting different origins. Then, it will read the value of Access-Control-Allow-Origin and based on that, it
 will assess if CORS is properly configured.
-'''
+"""
+
 
 import urlparse
 alertTitle = 'Corsair - CORS misconfigured.'
@@ -19,17 +21,15 @@ SM_ORIGIN = 'https://scarymonster.com'
 SM_ORIGIN_NO_PROTOCOL = 'scarymonster.com'
 SM_ORIGIN_DOMAIN = 'scarymonster'
 
-def scanNode(sas, msg):
-
-'''
+"""
 In this method, we read the request performed and create a new request with a fake origin. Also, if the request already contains an origin header, we perform two extra requests:
  1 - Fake subdomain. It is, we set as origin a fake subdomain to validate if CORS is configured at domain level, or if it has granularity at subdomain level.
  2 - Fake postdomain. Here, we set a request using the existing origin as a subdomain for a fake domain. We do it to validate if CORS is misconfigured and it only checks if the expected value exists in the origin header.
 Then, we call cors_scan, which will send that request and validate the response
-'''
+"""
+def scanNode(sas, msg):
   origMsg = msg
-  msg = origMsg.cloneRequest()
-  
+  msg = origMsg.cloneRequest() 
   msg.getRequestHeader().setHeader("Origin", SM_ORIGIN)
   cors_scan(sas,msg, "fake origin")
   if origMsg.getRequestHeader().getHeader('Origin'):
@@ -41,10 +41,10 @@ Then, we call cors_scan, which will send that request and validate the response
     msg.getRequestHeader().setHeader("Origin", postdomain)
     cors_scan(sas,msg, "fake postdomain")
 
+"""
+cors_scan sends the request crafted by scanNode and reads the value of the Access-Control-Allow-Origin header. If it is the origin reflected, * or NULL, we consider that CORS is misconfigured and raise an
+"""
 def cors_scan(sas,msg, test_type):
-'''
-cors_scan sends the request crafted by scanNode and reads the value of the Access-Control-Allow-Origin header. If it is the origin reflected, * or NULL, we consider that CORS is misconfigured and raise an alert
-'''
   sas.sendAndReceive(msg, True, False);
   header = str(msg.getResponseHeader().getHeader("Access-Control-Allow-Origin"))
   if (header in ['null', '*', msg.getRequestHeader().getHeader('Origin')]):
